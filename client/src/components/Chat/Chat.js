98a78c52e4a4bf731a74beca9525bd677e01b38a
T-Damer <dpancov64@gmail.com>
2021-08-20 17:10:50 +0300

    established conneciton between client and server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 import './Chat.css'
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'localhost:5000'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(
+      location.search
+    )
+
+    let socket = io(ENDPOINT)
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit()
+  }, [ENDPOINT, location.search])
+
   return <h1>Chat</h1>
 }
 
